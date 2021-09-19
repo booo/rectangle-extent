622096153a03b21406c5dbf8862a6f2291a0ef20
Philipp Borgers <borgers@mi.fu-berlin.de>
2021-09-19 15:30:02 +0200

    place the center of the rectangle at the input position

--- rectangle-extent.py
+++ rectangle-extent.py
@@ -1,24 +1,23 @@
 from geojson import Polygon, Feature, FeatureCollection
 from geopy import distance
+from math import sqrt
 import argparse
 import json
 import simplekml
 
-
 def calculate_rect(lat, lon, dist):
-    # Calculate Rectangle with south west in Berlin with width 10km  and height 2km 
-    south_west = (lat, lon, 0)
-    width_in_km = dist
-    height_in_km = dist
+    center = (lat, lon, 0)
+
+    # calculate distance from center to edge
+    dist = dist / 2
 
     # Bearing in degrees: 0 – North, 90 – East, 180 – South, 270 or -90 – West.
-    bearing_east = 90
-    bearing_north = 0
+    dist = distance.distance(kilometers=sqrt(dist ** 2 + dist ** 2))
 
-    south_east = distance.distance(kilometers=width_in_km).destination(south_west, bearing_east)
-    north_east = distance.distance(kilometers=height_in_km).destination(south_east, bearing_north)
-    north_west = distance.distance(kilometers=height_in_km).destination(south_west, bearing_north)
-
+    south_west = dist.destination(center, 90 + 45)
+    north_west = dist.destination(center, 0 + 45)
+    north_east = dist.destination(center, 0 - 45)
+    south_east = dist.destination(center, 180 + 45)
 
     # flip lat/long to lon/lat (geojson standard)
     south_west_lonlat = south_west[:2][::-1]
